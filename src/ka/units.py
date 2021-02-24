f6f2dcce1b7916d51ef5fd96a719723fb2083925
Kevin Galligan <galligankevinp@gmail.com>
2021-02-24 23:35:34 +0000

    Adds a bunch of units.

--- src/ka/units.py
+++ src/ka/units.py
@@ -85,6 +85,9 @@ class QuantitySpace:
             self.base_units)
 
 class Unit:
+
+    NO_PLURAL = "noplural"
+
     def __init__(self, symbol, singular_name, plural_name, quantities,
                  quantity_vector, multiple, offset):
         self.symbol = symbol
@@ -124,25 +127,20 @@ def register_unit(symbol,
         QV_TO_QUANTITY[quantity_vector].append(q)
     
     assert singular_name not in NAME_TO_UNIT
-    assert plural_name not in NAME_TO_UNIT
     assert symbol not in SYMBOL_TO_UNIT
     NAME_TO_UNIT[singular_name] = unit
-    NAME_TO_UNIT[plural_name] = unit
     SYMBOL_TO_UNIT[symbol] = unit
+
+    if plural_name != Unit.NO_PLURAL:
+        assert plural_name not in NAME_TO_UNIT
+        NAME_TO_UNIT[plural_name] = unit
 
     # return this so that it can be reused when
     # defining other units.
     return quantity_vector
 
-class Quantities:
-    TIME = "time"
-    LENGTH = "length"
-    MASS = "mass"
-    ELECTRIC_CURRENT = "electric current"
-    THERMODYNAMIC_TEMP = "thermodynamic temperature"
-    AMOUNT_OF_SUBSTANCE = "amount of substance"
-    LUMINOUS_INTENSITY = "luminous intensity"
-
+## Here's the "space" based on the base units we wanna use.
+## All quantities exist within this space.
 QSPACE = QuantitySpace(["kg", "m", "s", "A", "K", "mol", "cd"])
 KG = QSPACE.get_basis_vector("kg")
 M = QSPACE.get_basis_vector("m")
@@ -152,20 +150,51 @@ K = QSPACE.get_basis_vector("K")
 MOL = QSPACE.get_basis_vector("mol")
 CD = QSPACE.get_basis_vector("cd")
 
-## Base units.
-register_unit("s", "second", Quantities.TIME, S)
-register_unit("m", "metre", Quantities.LENGTH, M)
+## SI base units.
+register_unit("s", "second", "time", S)
+register_unit("m", "metre", "length", M)
 # Nice edge case, Obama.
-register_unit("g", "gram", Quantities.MASS, KG, multiple=rat(1,1000))
-register_unit("A", "ampere", Quantities.ELECTRIC_CURRENT, A)
-register_unit("K", "kelvin", Quantities.THERMODYNAMIC_TEMP, K)
-register_unit("mol", "mole", Quantities.AMOUNT_OF_SUBSTANCE, MOL)
+register_unit("g", "gram", "mass", KG, multiple=rat(1,1000))
+register_unit("A", "ampere", "electric current", A)
+register_unit("K", "kelvin", "thermodynamic temperature", K)
+register_unit("mol", "mole", "amount of substance", MOL)
 # The worst unit, apparently.
-register_unit("cd", "candela", Quantities.LUMINOUS_INTENSITY, CD)
+register_unit("cd", "candela", "luminous intensity", CD)
 
-# TODO: write a scraper for these Wikipedia pages to get all the units.
-# As much as possible. Otherwise, I'll have a lot of tedious typing to
-# do. There may be other "fun" units I can incorporate.
+## Named units, derived from SI base units.
+## https://en.wikipedia.org/wiki/SI_derived_unit
+## If I don't make a mistake here somewhere, it'll be a miracle.
+## I should really write a script to parse the wiki.
+register_unit("Hz", "hertz", "frequency", S**-1, plural_name=Unit.NO_PLURAL)
+register_unit("rad", "radian", "angle", M / M)
+register_unit("sr", "steradian", "solid angle", M**2 / M**2)
+register_unit("N", "newton", ["force", "weight"], KG * M * S**-2)
+register_unit("Pa", "pascal", ["pressure", "stress"], KG * M**-1 * S**-2)
+J = register_unit("J", "joule", ["energy", "work", "heat"], KG * M**2 * S**-2)
+register_unit("W", "watt", ["power", "radiant flux"], KG * M**2 * S**-3)
+C = register_unit("C", "coulomb", ["electric charge", "quantity of electricity"], S * A)
+V = register_unit("V", "volt", ["voltage", "electrical potential difference", "electromotive force"], J / C)
+register_unit("F", "farad", "electrical capacitance", C / V)
+register_unit("Ω", "ohm", "electrical resistance", V / A)
+register_unit("S", "siemens", "electrical conductance", A / V, plural_name=Unit.NO_PLURAL)
+register_unit("Wb", "weber", "magnetix flux", V * S)
+register_unit("T", "tesla", ["magnetic induction", "magnetic flux density", KG * S**-2 * A**-1)
+register_unit("H", "henry", "electrical inductance", V * S / A, plural_name="henries")
+# Degrees celcius is offset from 0 kelvin by -273.15 = -5463/20.
+register_unit("°C", "degC", "thermodynamic temperature", K, offset=-rat(5463, 20))
+register_unit("lm", "lumen", "luminous flux", CD)
+register_unit("lx", "lux", "illuminance", CD * M**-2, plural_name=Unit.NO_PLURAL)
+register_unit("Bq", "becquerel", "radioactivity", S**-1)
+register_unit("Gy", "gray", "absorbed dose", J / KG)
+register_unit("Sv", "sievert", "equivalent dose", J / KG)
+register_unit("kat", "katal", "catalytic activity", MOL * S**-1)
+
+# TODO
+# Add all these units:
 #   https://en.wikipedia.org/wiki/International_System_of_Units
-#   https://en.wikipedia.org/wiki/SI_derived_unit
 #   https://en.wikipedia.org/wiki/Non-SI_units_mentioned_in_the_SI
+# Then run some tests to validate the units.
+# Check if that all words used in quantities are in the dictionary.
+# Check that all symbol / unit names are on the wiki page.
+# Check that no quantities have a distance of <=2 (or sth) from each other.
+# And so on.
