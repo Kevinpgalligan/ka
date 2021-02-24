a98b3de9806a3d27bb70a125e37d34a9f74ab6b4
Kevin Galligan <galligankevinp@gmail.com>
2021-02-24 22:00:49 +0000

    Finishes most of the design work for units.

--- src/ka/units.py
+++ src/ka/units.py
@@ -1,50 +1,16 @@
 from .functions import multiply
-from .types import number
 
-class Quantities:
-    TIME = "time"
-    LENGTH = "length"
-    MASS = "mass"
-    ELECTRIC_CURRENT = "electric current"
-    THERMODYNAMIC_TEMP = "thermodynamic temperature"
-    AMOUNT_OF_SUBSTANCE = "amount of substance"
-    LUMINOUS_INTENSITY = "luminous intensity"
+import collections
+from fractions import Fraction as frac
 
-# Need to define:
-# 1. multiplication & so on, defined in terms of other units & in terms
-#    of numbers.
-# 2. a way of simplifying the unit and extracting the multiplier?
-#    so, when creating a quantity, you wanna simplify the unit by
-#    taking the multiplier and applying it to the magnitude/value
-#    of the quantity.
-# I'm thinking through cases here.
-# 5 feet^2: first, we fetch the feet unit, which already has a multiplier
-#           applied to metres. Then we square it, which affects the multipler
-#           and the dimension signature / whatever. Then we form a quantity
-#           by applying the multiplier and getting it in terms of base units.
-#           The unit of a quantity may not have a name!
-#           For example, what about 5 feet^2 s^-1? It doesn't have a symbol.
-#           I think a quantity might end up having a unit signature rather than
-#           a unit. We can fetch a unit / name for it when displaying.
-# THIS IS SO DIFFICULT TO MODEL.
-#class Unit:
-#    def __init__(self, symbol, names, quantity, multiplier=number(1)):
-#        self.symbol = symbol
-#        self.names = names
-#        self.quantity = quantity
-#        self.multiplier = multiplier
-#
-#UNIT_MAP = {}
-#
-#def register_unit(symbol, singular_name, quantity):
-#    global UNIT_MAP
-#    unit = Unit(symbol, [singular_name, singular_name+"s"], quantity)
-#
-#
-#S = register_unit("s", "second", "time")
+QUANTITY_TO_QV = {}
+QV_TO_QUANTITY = collections.defaultdict(list)
+NAME_TO_UNIT = {}
+SYMBOL_TO_UNIT = {}
 
 class Vector:
     def __init__(self, xs):
+        assert isinstance(xs, tuple)
         self.xs = xs
 
     def __len__(self):
@@ -54,19 +20,28 @@ class Vector:
         return isinstance(other, Vector) and all(x==y for x, y in zip(self.xs, other.xs))
 
     def __add__(self, other):
-        return Vector([x+y for x, y in zip(self.xs, other.xs)])
+        return Vector(tuple(x+y for x, y in zip(self.xs, other.xs)))
 
     def __rmul__(self, a):
         return self.__mul__(a)
 
     def __mul__(self, a):
-        return Vector([a*x for x in self.xs])
+        return Vector(tuple(a*x for x in self.xs))
+
+    def __neg__(self):
+        return Vector(tuple(-x for x in self.xs))
 
     def __str__(self):
         return "<" + ",".join(str(x) for x in self.xs) + ">"
 
     def __repr__(self):
         return str(self)
+
+    def __hash__(self):
+        return hash(self.xs)
+
+    def __eq__(self, other):
+        return isinstance(other, Vector) and self.xs == other.xs
 
     def __iter__(self):
         return iter(self.xs)
@@ -85,6 +60,15 @@ class QuantityVector:
     def __pow__(self, a):
         return QuantityVector(a*self.v, self.names)
 
+    def __div__(self, other):
+        return self * QuantityVector(-other.v, other.names)
+
+    def __eq__(self, other):
+        return isinstance(other, QuantityVector) and self.v == other.v
+
+    def __hash__(self):
+        return hash(self.v)
+
     def prettified(self):
         return " ".join(f"{name}^{exp}" if exp != 1 else name
                         for exp, name in zip(self.v, self.names)
@@ -97,8 +81,67 @@ class QuantitySpace:
     def get_basis_vector(self, basis_name):
         assert basis_name in self.base_units
         return QuantityVector(
-            Vector([1 if name==basis_name else 0 for name in self.base_units]),
+            Vector(tuple(1 if name==basis_name else 0 for name in self.base_units)),
             self.base_units)
+
+class Unit:
+    def __init__(self, symbol, singular_name, plural_name, quantities,
+                 quantity_vector, multiple, offset):
+        self.symbol = symbol
+        self.singular_name = singular_name
+        self.plural_name = plural_name
+        self.quantities = quantities
+        self.quantity_vector = quantity_vector
+        self.multiple = multiple
+        self.offset = offset
+
+def register_unit(symbol,
+                  singular_name,
+                  quantities,
+                  quantity_vector,
+                  plural_name=None,
+                  multiple=1,
+                  offset=0):
+    if plural_name is None:
+        plural_name = singular_name + "s"
+    if isinstance(quantities, str):
+        quantities = [quantities]
+
+    unit = Unit(symbol, singular_name, plural_name, quantities,
+                quantity_vector, multiple, offset)
+
+    global QUANTITY_TO_QV, QV_TO_QUANTITY, NAME_TO_UNIT, SYMBOL_TO_UNIT
+    for q in quantities:
+        if q in QUANTITY_TO_QV:
+            # Each quantity name can be associated with only
+            # 1 type of quantity vector. Unless you include
+            # weird non-SI units like second/minute, which refer
+            # to both time and plane angle. But I'm not going
+            # to include them.
+            assert quantity_vector == QUANTITY_TO_QV[q]
+        else:
+            QUANTITY_TO_QV[q] = quantity_vector
+        QV_TO_QUANTITY[quantity_vector].append(q)
+    
+    assert singular_name not in NAME_TO_UNIT
+    assert plural_name not in NAME_TO_UNIT
+    assert symbol not in SYMBOL_TO_UNIT
+    NAME_TO_UNIT[singular_name] = unit
+    NAME_TO_UNIT[plural_name] = unit
+    SYMBOL_TO_UNIT[symbol] = unit
+
+    # return this so that it can be reused when
+    # defining other units.
+    return quantity_vector
+
+class Quantities:
+    TIME = "time"
+    LENGTH = "length"
+    MASS = "mass"
+    ELECTRIC_CURRENT = "electric current"
+    THERMODYNAMIC_TEMP = "thermodynamic temperature"
+    AMOUNT_OF_SUBSTANCE = "amount of substance"
+    LUMINOUS_INTENSITY = "luminous intensity"
 
 QSPACE = QuantitySpace(["kg", "m", "s", "A", "K", "mol", "cd"])
 KG = QSPACE.get_basis_vector("kg")
@@ -109,8 +152,20 @@ K = QSPACE.get_basis_vector("K")
 MOL = QSPACE.get_basis_vector("mol")
 CD = QSPACE.get_basis_vector("cd")
 
-# API:
-#   register_unit("N", "newton", Measures.FORCE, KG * M * S**-2)
-# Also, g can be a unit, it just needs a multiple of 1/1000. kg doesn't
-# need to be defined as its own unit. And then the kilo prefix will UNDO
-# that multiple. PERFECT. THE PROBLEM IS SOLVED!
+## Base units.
+register_unit("s", "second", Quantities.TIME, S)
+register_unit("m", "metre", Quantities.LENGTH, M)
+# Nice edge case, Obama.
+register_unit("g", "gram", Quantities.MASS, KG, multiple=rat(1,1000))
+register_unit("A", "ampere", Quantities.ELECTRIC_CURRENT, A)
+register_unit("K", "kelvin", Quantities.THERMODYNAMIC_TEMP, K)
+register_unit("mol", "mole", Quantities.AMOUNT_OF_SUBSTANCE, MOL)
+# The worst unit, apparently.
+register_unit("cd", "candela", Quantities.LUMINOUS_INTENSITY, CD)
+
+# TODO: write a scraper for these Wikipedia pages to get all the units.
+# As much as possible. Otherwise, I'll have a lot of tedious typing to
+# do. There may be other "fun" units I can incorporate.
+#   https://en.wikipedia.org/wiki/International_System_of_Units
+#   https://en.wikipedia.org/wiki/SI_derived_unit
+#   https://en.wikipedia.org/wiki/Non-SI_units_mentioned_in_the_SI
